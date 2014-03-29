6aee1428299b9b7b490e1ee502a2114756704ed6
Evan Tahler <evan@taskrabbit.com>
2014-03-29 17:10:46 -0400

    logger

--- config/logger.js
+++ config/logger.js
@@ -42,4 +42,35 @@ exports.test = {
   }
 }
 
- 
+exports.production = { 
+  logger: function(api){
+    var logger = { transports: [] };
+
+    // console logger
+    if(cluster.isMaster){
+      logger.transports.push(function(api, winston){
+        return new (winston.transports.Console)({
+          colorize: true,
+          level: 'info',
+          timestamp: api.utils.sqlDateTime
+        });
+      });
+    }
+
+    // file logger
+    try{
+      fs.mkdirSync('./log');
+    } catch(e) {
+      if(e.code != 'EEXIST'){ console.log(e); process.exit(); }
+    }
+    logger.transports.push(function(api, winston) {
+      return new (winston.transports.File)({
+        filename: api.config.general.paths.log[0] + '/' + api.pids.title + '.log',
+        level: 'info',
+        timestamp: true
+      });
+    });
+
+    return logger;
+  }
+}
